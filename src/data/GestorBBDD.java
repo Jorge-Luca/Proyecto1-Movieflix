18a5794053204905ddd3bc0e9239e8db69572149
Jorge <pruebasluca@gmail.com>
2019-09-30 20:49:30 +0200

    Introduce documentacion de la clase GestorBBDD y su método estableceConexion()

--- src/data/GestorBBDD.java
+++ src/data/GestorBBDD.java
@@ -14,9 +14,12 @@ public class GestorBBDD {
 	}
 	
 	public static Connection estableceConexion() {
-		
+		/*@author Jorge
+		 * 
+		 * Método diseñado para devolver una conexion abierta con la BBDD
+		 */
 		Connection con = null;
-		String url = "jdbc:mysql://80.65.12.133:3306/nombre_schema?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+		String url = "jdbc:mysql://80.65.12.133:3306/proyecto1?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
         String user = "root";
         String password = "root";
 			
