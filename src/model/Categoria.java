9172f8e95248989cec344ca586dd518c668e97eb
Yolanda <yolyguti79@hotmail.com>
2019-09-30 16:43:05 +0200

    merging a model

---
+++ src/model/Categoria.java
@@ -0,0 +1,10 @@
+package model;
+
+public enum Categoria {
+	
+	// valor de las constantes que recoge cada categoria y cada pelicula
+	
+	POLICIACA,ROMANTICA, AVENTURA, COMEDIA, ANIMACION,THRILLER;
+	
+	
+}
