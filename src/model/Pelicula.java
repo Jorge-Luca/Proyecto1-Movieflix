cc09af557e21959a23d1cbe6c220bb7f54e307dc
IVAN <ivan.job@outlook.es>
2019-09-30 16:48:48 +0200

    Se ha creado la clase pelicula, junto con los atributos, getters y setters, constructores y toString

---
+++ src/model/Pelicula.java
@@ -0,0 +1,61 @@
+package model;
+/* Esta es la clase Pelicula. 
+ * Incluye los atributos, constructores, getters, setters, y toString, necesarios.
+ * 
+ */
+public class Pelicula {
+	private String nombre;
+	private int angnoEstreno;
+	private Categoria categoria;
+	
+	public Pelicula() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	public Pelicula(String nombre, int angnoEstreno, Categoria categoria) {
+		super();
+		this.nombre = nombre;
+		this.angnoEstreno = angnoEstreno;
+		this.categoria = categoria;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public int getAngnoEstreno() {
+		return angnoEstreno;
+	}
+
+
+	public void setAngnoEstreno(int angnoEstreno) {
+		this.angnoEstreno = angnoEstreno;
+	}
+
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+
+	@Override
+	public String toString() {
+		return "Pelicula [nombre=" + nombre + ", angnoEstreno=" + angnoEstreno + "]";
+	}
+
+	
+}
