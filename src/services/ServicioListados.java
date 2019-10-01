5b9ea8c68908f2700f2d169c1a154f27e796379c
Yolanda <yolyguti79@hotmail.com>
2019-10-01 12:03:56 +0200

    Creada clase ServicioListados en package services

---
+++ src/services/ServicioListados.java
@@ -0,0 +1,14 @@
+package services;
+
+/*@author Yolanda
+ * 
+ * Creamos la clase ServicioListados para crear los metodos que unen  packages
+ * data gestor BBDD con el package control.
+ */
+
+
+public class ServicioListados {
+	
+	
+
+}
