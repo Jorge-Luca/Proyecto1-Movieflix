de6f49db0fabbab7a078c1cf950c7e76a976d576
IVAN <ivan.job@outlook.es>
2019-10-01 16:11:49 +0200

    metodo devuelveListaAccesibles()

--- src/services/ServicioListados.java
+++ src/services/ServicioListados.java
@@ -1,14 +1,29 @@
 package services;
 
+import java.util.ArrayList;
+
+import data.GestorBBDD;
+import model.Pelicula;
+import model.Usuario;
+
 /*@author Yolanda
  * 
  * Creamos la clase ServicioListados para crear los metodos que unen  packages
  * data gestor BBDD con el package control.
  */
 
-
+/**
+ * @author Iván
+ * 
+ * El siguiente método se encarga de recibir el método devuelveListaAccesibles() para 
+ * almacenarlo en un array list y devolverlo para modularizar.
+ */
 public class ServicioListados {
 	
-	
+	public static ArrayList<Pelicula>devuelveListaAccesibles(Usuario u) {
+		ArrayList<Pelicula> aLDevolver = new ArrayList<Pelicula>();
+		aLDevolver=GestorBBDD.devuelveListaAccesibles();
+		return aLDevolver;
+	}
 
 }
