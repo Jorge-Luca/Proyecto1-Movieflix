785d996b8d194489d38a6c92ff97d6339c9fdee9
Yolanda <yolyguti79@hotmail.com>
2019-10-01 11:35:17 +0200

    He creado las clases en el package Servicios y  los metodos que conectan con las BBDD

---
+++ src/services/ServicioUsuario.java
@@ -0,0 +1,28 @@
+package services;
+
+import model.Pelicula;
+import data.Usuario;
+
+/*@author Yolanda
+ * 
+ * Creamos la clase ServicioUsario para crear los metodos que unen  packages
+ * data gestor BBDD con el package control.
+ */
+
+public class ServicioUsuario {
+	
+	public ServicioUsuario () {
+		
+		/*@utor Yolanda
+		 * Creamos método insertaPelicula que muestra los datos insertarUsuario del GestorBBDD
+		 */
+		
+		public static void insertarUsuario (Usuario) {
+			
+			GestorBBDD.insertarUsuario (u);
+			
+		}
+	
+
+}
+}
