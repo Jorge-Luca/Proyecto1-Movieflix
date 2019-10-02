33345e4a9674b0ba561db650ceeae201407d8486
Yolanda <yolyguti79@hotmail.com>
2019-10-02 12:58:13 +0200

    Cambiios

--- src/services/ServicioListados.java
+++ src/services/ServicioListados.java
@@ -6,12 +6,6 @@ import data.GestorBBDD;
 import model.Pelicula;
 import model.Usuario;
 
-/*@author Yolanda
- * 
- * Creamos la clase ServicioListados para crear los metodos que unen  packages
- * data gestor BBDD con el package control.
- */
-
 /**
  * @author Iván
  * 
@@ -20,8 +14,20 @@ import model.Usuario;
  */
 public class ServicioListados {
 	
-	public static ArrayList<Pelicula> devuelveListaAccesibles(Usuario u) {
-		return GestorBBDD.devuelveListaAccesibles(u);
+	public static ArrayList<Pelicula>devuelveListaAccesibles(Usuario u) {
+		ArrayList<Pelicula> aLDevolver = new ArrayList<Pelicula>();
+		aLDevolver=GestorBBDD.devuelveListaAccesibles(u);
+		return aLDevolver;
+		
+		/*@author Yolanda
+		 * 
+		 * El siguiente método se encarga de recibir el método devuelveListaAccesibles() para 
+		 * almacenarlo en un array list y devolverlo para modularizar.
+		 */
+		public static ArrayList<Pelicula> devuelveListaCompletaPeliculas () {
+			return GestorBBDD.devuelveListaCompletaPeliculas();
+			
+		
 	}
-
+}
 }
