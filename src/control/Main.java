878d3d9035d4f5aeeb92a14c6f891c8a5567ff92
IVAN <ivan.job@outlook.es>
2019-10-02 10:49:05 +0200

    copia1

--- src/control/Main.java
+++ src/control/Main.java
@@ -1,9 +1,12 @@
 package control;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		private static final Logger logger = LogManager.getLogger("");
 	}
 }
